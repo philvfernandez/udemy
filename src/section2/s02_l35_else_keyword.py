2ef4f07e39fb6ad4092b2f7b0115aca8d7c29c8a
Felipe.Fernandez <philvfernandez@gmail.com>
2025-02-27 15:15:33 -0700

    Code for section 2 lesson 5 on using else keyword with a loop.

---
+++ src/section2/s02_l35_else_keyword.py
@@ -0,0 +1,12 @@
+cars = ["ok", "ok", "ok", "ok", "faulty", "ok", "ok"]
+
+for status in cars:
+    if status == "faulty":
+        ##print("Stopping the production line!")
+        ##break
+        print("Stopping the production line!!")
+        break
+    print(f"This car is {status}")
+    print("Shipping new car to customer!")
+else:
+    print("All cars built successfully.  No faulty cars!!")

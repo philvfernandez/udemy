20802e2ed814623801577757675bf0cca394659a
Felipe.Fernandez <philvfernandez@gmail.com>
2025-02-27 16:12:15 -0700

    Code for section 2 lesson 37 - Using list slicing.

---
+++ src/section2/s02_l37_list_slicing.py
@@ -0,0 +1,25 @@
+friends = ["Ralf", "Charlie", "Anna", "Bob", "Jen"]
+
+## List slicing
+print(friends[2:4])
+
+# All elements between 1 and n
+print(friends[1:])
+
+# All elements except the last one.
+print(friends[:4])
+
+## All elements in a new list
+print(friends[:])
+
+## Elements starting from length of list -3
+print(friends[-3:])
+
+## Elements starting from the length of list -3 to the fourth element
+print(friends[-3:4])
+
+## Starts at the start of list to the length -2 element(s) of the list.
+print(friends[:-2])
+
+## From the length -3 element to the length element of the list.
+print(friends[-3:-1])

df150379869b74093e2d65ee83b6c326e9f18fde
Felipe.Fernandez <philvfernandez@gmail.com>
2025-03-21 15:58:12 -0600

    Example of passing a list of cars into the function.

--- src/section2/s02_l45_args_and_params.py
+++ src/section2/s02_l45_args_and_params.py
@@ -1,3 +1,9 @@
+cars = [
+    {"make": "Ford", "model": "Fiesta", "mileage": 23000, "fuel_consumed": 460},
+    {"make": "Ford", "model": "Focus", "mileage": 17000, "fuel_consumed": 350},
+    {"make": "Mazda", "model": "MX-5", "mileage": 49000, "fuel_consumed": 900},
+    {"make": "Mini", "model": "Cooper", "mileage": 31000, "fuel_consumed": 235},
+]
 
 
 def calculate_mpg(car_to_calculate):
@@ -5,10 +11,5 @@ def calculate_mpg(car_to_calculate):
     name = f"{car_to_calculate['make']} {car_to_calculate['model']}"
     print(f"{name} does {mpg} miles per gallon.")
 
-
-calculate_mpg({
-    "make": "Ford",
-    "model": "Fiesta",
-    "mileage": 23000,
-    "fuel_consumed": 460
-})
+for car in cars:
+    calculate_mpg(car)

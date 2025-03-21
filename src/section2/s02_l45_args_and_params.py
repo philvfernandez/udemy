b58383d26b77e4bde4b2952911be066a64ee1b43
Felipe.Fernandez <philvfernandez@gmail.com>
2025-03-21 15:48:52 -0600

    Example of using a function argument.

--- src/section2/s02_l45_args_and_params.py
+++ src/section2/s02_l45_args_and_params.py
@@ -1,13 +1,14 @@
-def calculate_mpg():
-    car = {
-        "make": "Ford",
-        "model": "Fiesta",
-        "mileage": 23000,
-        "fuel_consumed": 460
-    }
-    mpg = car["mileage"] / car["fuel_consumed"]
-    name = f"{car['make']} {car['model']}"
+
+
+def calculate_mpg(car_to_calculate):
+    mpg = car_to_calculate["mileage"] / car_to_calculate["fuel_consumed"]
+    name = f"{car_to_calculate['make']} {car_to_calculate['model']}"
     print(f"{name} does {mpg} miles per gallon.")
 
 
-calculate_mpg()
+calculate_mpg({
+    "make": "Ford",
+    "model": "Fiesta",
+    "mileage": 23000,
+    "fuel_consumed": 460
+})

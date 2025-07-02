72511638ca63400943b2b86f44c15ae3fa50cacc
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-02 13:58:38 -0600

    Refactored grades function example into a basic class.

--- src/section4/s04_58_oo_programming.py
+++ src/section4/s04_58_oo_programming.py
@@ -2,7 +2,7 @@
 my_student = {
     'name' : 'Phil Fernandez',
     'grades' : [70, 88, 90, 99],
-    'average' : '' ## something that calculates average
+    'average' : None ## something that calculates average
 }
 
 def calculate_grades(my_student):
@@ -18,3 +18,26 @@ print(calculate_grades(my_student))
 
 ## If we added an average to the dictionary, it would need to be a function that calculates the average and still
 ## be part of the dictionary.  However, that is not possible.  So we will need to have classes and objects.
+
+## An object stores data and actions that operate on that data.
+
+## The functions inside a class that start with '__' are called doneder functions. THey are special python functions.
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+## Creates an object of type Student
+student_one = Student('Tawnee Fernandez', [70, 88 ,90, 99])
+student_two = Student('Jose', [50, 68, 99, 100])
+
+print(student_one.name)
+print(student_two.name)
+
+## Prints the type of the class which in this case is of type Student
+print(student_one.__class__)
+

2449124891874da2a77e1e54f117dd0297fb2c17
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-03 14:56:44 -0600

    Added a WorkingStudent class that inherits from the Student class.

--- src/section4/s04_66_inheritance.py
+++ src/section4/s04_66_inheritance.py
@@ -7,16 +7,28 @@ class Student:
     def average(self):
         return sum(self.marks) / len(self.marks)
 
-class WorkingStudent:
+## WorkingStudent is a child of Student or it extends Student
+class WorkingStudent(Student):
     def __init__(self, name, school, salary):
-        self.name = name
-        self.school = school
-        self.marks = []
-        self.salary = salary
+        ## Super is the parent class (Student)
+       super().__init__(name, school)
+       self.salary = salary
 
-    def average(self):
-        return sum(self.marks) / len(self.marks)
+    def weeklySalary(self):
+        return self.salary * 37.5
+
 
 
 phil = WorkingStudent('Phil', 'MIT', 15.50)
 print(phil.salary)
+phil.marks.append(57)
+phil.marks.append(99)
+print(phil.average())
+print(phil.weeklySalary())
+
+
+## This will return an error because there is no weekly_salary defined in the Student class.  It's only defined
+## in the Working_Student class.  Which is to say that WorkingStudent gets stuff from Student.  And not the other
+## way around.
+## anna = Student('Anna', 'Oxford')
+## print(anna.weekly_salary())

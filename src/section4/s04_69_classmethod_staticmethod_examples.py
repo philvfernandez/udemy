c63a5128dd8bcffa13c8bc6b8ed6dca2d9b9556a
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-03 15:41:21 -0600

    Refactored first example to use a staticmethod decorator on the from_sum method

--- src/section4/s04_69_classmethod_staticmethod_examples.py
+++ src/section4/s04_69_classmethod_staticmethod_examples.py
@@ -6,9 +6,9 @@ class FixedFloat:
     def __repr__(self):
         return f'<FixedFloat {self.amount:.2f}>'
 
-    def from_sum(self, value1, value2):
+    @staticmethod
+    def from_sum(value1, value2):
         return FixedFloat(value1 + value2)
 
-number = FixedFloat(18.5746)
-new_number = number.from_sum(19.575, 0.789)
+new_number = FixedFloat.from_sum(19.575, 0.789)
 print(new_number)

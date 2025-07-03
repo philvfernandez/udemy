cfa9c1fa8c2d99a7e70a946ec28f3192a1554275
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-03 14:44:50 -0600

    Basic example w/o inheritance that was used in the start of the lecture.

---
+++ src/section4/s04_66_inheritance.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+class WorkingStudent:
+    def __init__(self, name, school, salary):
+        self.name = name
+        self.school = school
+        self.marks = []
+        self.salary = salary
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+
+phil = WorkingStudent('Phil', 'MIT', 15.50)
+print(phil.salary)

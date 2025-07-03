058c218aec68d0809c8c2734763063ee78bfb91e
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-03 15:53:46 -0600

    Added better example that uses both the static and class method decorators to show when to use each.

--- src/section4/s04_69_classmethod_staticmethod_examples.py
+++ src/section4/s04_69_classmethod_staticmethod_examples.py
@@ -6,9 +6,28 @@ class FixedFloat:
     def __repr__(self):
         return f'<FixedFloat {self.amount:.2f}>'
 
-    @staticmethod
-    def from_sum(value1, value2):
-        return FixedFloat(value1 + value2)
+    @classmethod
+    def from_sum(cls, value1, value2):
+        return cls(value1 + value2)
 
 new_number = FixedFloat.from_sum(19.575, 0.789)
 print(new_number)
+
+class Euro(FixedFloat):
+    def __init__(self, amount):
+        super().__init__(amount)
+        self.symbol = '€'
+
+    def __repr__(self):
+        return f'<Euro {self.symbol}{self.amount:.2f}>'
+
+
+money = Euro(18.786)
+print(money)
+
+## This will use the from_sum method from the parent FixedFloat class inherited from the Euro class.
+## But we don't want this.  We want an object of the type we called it with, which is Euro.
+## In order to do that, we need to change the FixedFloat from_sum method to use a classmethod decorator
+## instead of a staticmethod decorator.
+money = Euro.from_sum(19.575, 0.789)
+print(money)

4c499db8f6991624e1f0529d43cdfcb51c15d1aa
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-09 15:27:02 -0600

    Added example code from lecture.

---
+++ src/section5/s05_l77_dealing_with_python_errors.py
@@ -0,0 +1,32 @@
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def __repr__(self):
+        return f'<Car {self.make} {self.model}'
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    def __len__(self):
+        return len(self.cars)
+
+    def add_car(self, car):
+        if not isinstance(car, Car):
+            raise TypeError(f'Tried to add a `{car.__class__.__name__}` to the garage, but you can only add a `Car` objects')
+        self.cars.append(car)
+
+
+
+ford = Garage()
+mustang = Car('Ford', 'Mustang')
+try:
+    ford.add_car('Fiesta')
+except TypeError:
+    print('Your car was not a Car!')
+except ValueError:
+    print('Unexpected value error')
+finally:
+    print("This error happens every time regardless of any other error.")

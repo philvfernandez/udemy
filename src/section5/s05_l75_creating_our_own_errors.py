1142e2972446c16e697ab9100c67ae20d3a15272
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-09 14:47:02 -0600

    Added example code from second half of the lecture.

--- src/section5/s05_l75_creating_our_own_errors.py
+++ src/section5/s05_l75_creating_our_own_errors.py
@@ -1,6 +1,13 @@
-class MyCustomError(TypeError):
+class RuntimeErrorWithCode(TypeError):
+    """
+    Exception raised when a specific error code is needed.
+    """
     def __init__(self, message, code):
         super().__init__(f'Error code {code}: {message}')
         self.code = code
 
-raise MyCustomError("I am a custom error", 500)
+# raise MyCustomError("I am a custom error", 500)
+
+## Will print doc string given above
+error = RuntimeErrorWithCode('An error happened', 500)
+print(error.__doc__)

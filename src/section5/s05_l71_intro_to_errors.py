9399e11e327769ff38a687864ca4c391c917af5f
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-09 14:23:00 -0600

    Added example code for lecture 73 and raising errors.  I also renamed other python files in this section that were not named correctly.

---
+++ src/section5/s05_l71_intro_to_errors.py
@@ -0,0 +1,6 @@
+## Stack Trace
+## 1) At the very bottom it gives you the error that was raised and a description.
+## 2) What line of your code raised the error.
+## 3) What function that line is in.
+## 4) What function called the function that the line is in.
+## 5) And so on... until you reach the file that you executed.

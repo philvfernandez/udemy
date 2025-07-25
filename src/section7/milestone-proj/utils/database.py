4f140d8a941735e10c90976443710129ea9e2e60
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-25 13:53:34 -0600

    Added my solution for the milestone project 2 covered in brief, section 7/lecture 100.

--- src/section7/milestone-proj/utils/database.py
+++ src/section7/milestone-proj/utils/database.py
@@ -6,3 +6,23 @@ books = []
 
 def add_book(name, author):
     books.append({'name': name, 'author': author, 'read': False})
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book['name'] == name]
+
+def list_books():
+    global books
+    for book in books:
+        print(book)
+
+def prompt_read_book(book_name):
+    global books
+
+    for book in books:
+        if book['name'] == book_name:
+            book['read'] = True
+        else:
+            print('Book name Not Found.')
+
+

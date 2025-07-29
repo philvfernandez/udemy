18371aa9393ffb1a63ea70986e4e556cb60d224b
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-29 14:51:05 -0600

    Section 7, lecture 103.  Refactored the code to use JSON instead of a csv file.

--- src/section7/milestone-proj/utils/database.py
+++ src/section7/milestone-proj/utils/database.py
@@ -1,45 +1,46 @@
-"""
-Concerned with storing and retrieving books from a csv file.
-Format of the CSV file
+import json
 
-name, author, read\n
-name, author, read\n
-name, author, read\n
+"""
+Concerned with storing and retrieving books from a json file.
+Format of the JSON file
+
+[
+    {
+        'name': 'Clean Code',
+        'author': 'Robert'
+        'read': True
+    }
+]
 ...
 """
 
-books_file = 'books.csv'
+books_file = 'books.json'
 
 def create_book_table():
     with open(books_file, 'w') as file:
-        pass # just to make sure the file is created before we try to read all books.
+        json.dump([], file)
 
 def add_book(name, author):
-    with open(books_file, 'a') as file:
-        file.write(f'{name},{author},0\n')
+    books = get_all_books()
+    books.append({'name': name, 'author': author, 'read': False})
+    _save_all_books(books)
 
 def get_all_books():
     with open(books_file, 'r') as file:
-        lines = [line.strip().split(',') for line in file.readlines()] # [[name, author, read], [name, author, read]]
+        return json.load(file) ## Returns a list
 
-    return [ # [[name, author, read], [name, author, read]] --> list comprehension
-        {'name': line[0], 'author': line[1], 'read': int(line[2])}
-        for line in lines
-    ]
+def _save_all_books(books): # no private method in python so the convention is to prepend the method with a '_'.
+    with open(books_file, 'w') as file:
+        json.dump(books, file)
 
 def mark_book_as_read(name):
     books = get_all_books()
 
     for book in books:
         if book['name'] == name:
-            book['read'] = 1
+            book['read'] = True
 
     _save_all_books(books)
-
-def _save_all_books(books): # no private method in python so the convention is to prepend the method with a '_'.
-    with open(books_file, 'w') as file:
-        for book in books:
-            file.write(f"{book['name']},{book['author']},{book['read']}\n")
 
 def delete_book(name):
     books = get_all_books()

f81cb1f87d467cfc5b13a53f0da206b603604045
Felipe.Fernandez <philvfernandez@gmail.com>
2025-07-31 15:21:56 -0600

    s17:l108 - Refactored the add_books method to insert a new book record.  Also, excluded DB file in the .gitignore file.

--- src/section7/milestone-proj/utils/database.py
+++ src/section7/milestone-proj/utils/database.py
@@ -25,7 +25,7 @@ def create_book_table():
     # at most one writing data
     cursor = connection.cursor()
 
-    cursor.execute('CREATE TABLE books(name text primary key , author text, read integer)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key , author text, read integer)')
     # Commit means save the result of this query to disk.
     # Keep a bunch of data in memory until we commit.
     # We can write multiple things together, which is faster.
@@ -34,9 +34,23 @@ def create_book_table():
 
 
 def add_book(name, author):
-    books = get_all_books()
-    books.append({'name': name, 'author': author, 'read': False})
-    _save_all_books(books)
+    connection = sqlite3.connect('data.db')
+    # All operations in SQLite are made by cursors, and not by the connection object itself.
+    # That is so that we can have one single connection, but potentially multiple cursors either reading data and
+    # at most one writing data
+    cursor = connection.cursor()
+
+    cursor.execute('INSERT INTO books VALUES (?, ?, ?)', (name, author, 0))
+
+    # Second query that can be used instead of above sql query.
+    # Note: This is not recommended approach.
+    # f'INSERT INTO books VALUES("{name", "{author}", 0)')
+
+    # Commit means save the result of this query to disk.
+    # Keep a bunch of data in memory until we commit.
+    # We can write multiple things together, which is faster.
+    connection.commit()
+    connection.close()
 
 def get_all_books():
     with open(books_file, 'r') as file:

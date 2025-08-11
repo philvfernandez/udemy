89ea182318bdff6c866d108f154f9ba65a68a15c
Felipe.Fernandez <philvfernandez@gmail.com>
2025-08-11 11:18:39 -0600

    s09_l127 - Added additional notes from quiz and lectures.

--- src/section9/s09_l127_using_any_and_all_methods.py
+++ src/section9/s09_l127_using_any_and_all_methods.py
@@ -43,3 +43,40 @@ if(all(friends_nearby)): print('You are alone!')    # If one is true then they a
 
 #Example
 print(bool(0))
+b = -0.0
+# returns false because all non-zero numerical values evaluate to True, otherwise False.
+# -0.0 is the same as 0, so it would evaluate to False.
+print(bool(b))
+
+b2 = 0.00000000000000000000001
+# All non-zero numerical values evaluate to True, otherwise False.
+print(bool(b2))
+
+b3 = []
+# Lists, sets and dictionaries all evaluate to False if they are empty, otherwise True.
+print(bool(b3))
+
+b4 = [None]
+# Evaluates to True.  b is a list, and it only cares if it contains anything in itself when evaluating.
+# Even if it just contains a None, by itself is not empty anymore, thus it evaluates to True.
+print(bool(b4))
+
+"""
+Additional notes from quick and lectures:
+The previous quiz asked you a few questions about whether certain numbers or objects evaluate to True  or False . It's not really something you have to know by heart, but over time you'll just know whether something evaluates to True  or False .
+
+By the way, when we say "evaluates to True or False" we really mean this:
+
+my_list = []
+bool(my_list)  # False
+ 
+another_list = [None]
+bool(another_list)  # True
+Your classes in Python can implement a magic method, __bool__ , which will tell bool() , if statements, and the sort, whether your object should evaluate to True  or False .
+
+If you don't implement __bool__ , then __len__  will be used—if it returns 0  it will evaluate to False ; otherwise it will evaluate to True .
+
+Finally, if you don't implement either __bool__  or __len__ , your object will always evaluate to True .
+
+Learn more here: https://docs.python.org/3/library/stdtypes.html#truth-value-testing
+"""

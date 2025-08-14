b4a69209c65e1ef204e38bac686e15ad2e9752e6
Felipe.Fernandez <philvfernandez@gmail.com>
2025-08-14 14:17:49 -0600

    s10_l134 - Added notes from lectures.  No examples added to this file.

---
+++ src/section10/s101_134_queues.py
@@ -0,0 +1,9 @@
+# Queues - Add elements at the end.  Remove elements from the start.
+# .popleft() method removes element from the start of the queue.
+# .appendleft() method adds element to the left oof the queue.  -> If you think "right-to-left" instead, the
+# ends are opposed.
+# pop() method removes from the end.
+# Double-ended-queue (deque) - A queue where you can add or remove things from either side.
+# Otherwise, it's just a queue.
+
+# Stack - Add and remove elements from the top. (from the same end)

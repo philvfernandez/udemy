ca039905e6c0ca161ac264183d8732286caa3bcc
Felipe.Fernandez <philvfernandez@gmail.com>
2025-08-25 16:01:43 -0600

    s10_l141 - Added examples from lecture on regular expressions.

---
+++ src/section10/s10_l141_regular_expressions.py
@@ -0,0 +1,15 @@
+"""
+Example: given a pattern: a1 a3 a9 a5 a4
+Example: given a pattern: Jose, Rolf, Charlie, Adam
+Example: given a pattern: http://google.com
+Example: given a pattern: phil@google.com
+
+Four most important components:
+. - anything - letters, numbers, symbols.... but no newlines
++ "one or more of"
+* "zero or more of"
+? "zero or one of"
+
+.* --> zero or more of anything.
+.+ --> one or more of anything.
+"""

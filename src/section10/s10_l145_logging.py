b094481f701fa5c4658759b791b6e72c8b44219c
Felipe.Fernandez <philvfernandez@gmail.com>
2025-08-26 16:36:32 -0600

    s10_l145: Added additional logging examples and features from lecture 146.

--- src/section10/s10_l145_logging.py
+++ src/section10/s10_l145_logging.py
@@ -3,9 +3,19 @@ import logging
 #basic config
 #logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.DEBUG)
 # more readable config
-logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    level=logging.DEBUG,
+    filename='log.txt'
+)
 
 logger = logging.getLogger('test_logger')
+"""
+other ways to name your logger instance
+This will name the logger based on the module.
+logger = logging.getLogger(__name__) 
+"""
+
 
 # Note: Logging in Python is configured for warning and above by default.
 """

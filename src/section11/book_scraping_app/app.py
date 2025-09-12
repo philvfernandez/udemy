57d4685a790367f19fba22d39b2cb1f32a079974
Felipe.Fernandez <philvfernandez@gmail.com>
2025-09-12 14:33:39 -0600

    s11_l175 - Added code to get the page count from the book scraping website instead of setting the page count statically in the python code.

--- src/section11/book_scraping_app/app.py
+++ src/section11/book_scraping_app/app.py
@@ -7,7 +7,7 @@ page = AllBooksPage(page_content)
 
 books = page.books
 
-for page_num in range(1, 50):
+for page_num in range(1, page.page_count):
     url = f'https://books.toscrape.com/catalogue/page-{page_num+1}.html'
     page_content = requests.get(url).content
     page = AllBooksPage(page_content)

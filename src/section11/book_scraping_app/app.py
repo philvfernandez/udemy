5e30ace47088439416023b79bd685deda4af739d
Felipe.Fernandez <philvfernandez@gmail.com>
2025-09-12 11:16:04 -0600

    s11_l174 - Added code to get multiple pages from the books.toscrape.com website.

--- src/section11/book_scraping_app/app.py
+++ src/section11/book_scraping_app/app.py
@@ -7,3 +7,8 @@ page = AllBooksPage(page_content)
 
 books = page.books
 
+for page_num in range(1, 50):
+    url = f'https://books.toscrape.com/catalogue/page-{page_num+1}.html'
+    page_content = requests.get(url).content
+    page = AllBooksPage(page_content)
+    books.extend(page.books)

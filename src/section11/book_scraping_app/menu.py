c5e9f65c66f574049f73503d52fcb3dadda3226d
Felipe.Fernandez <philvfernandez@gmail.com>
2025-09-12 11:01:10 -0600

    s11_l171 - Added menu.

--- src/section11/book_scraping_app/menu.py
+++ src/section11/book_scraping_app/menu.py
@@ -1,5 +1,15 @@
 from app import books
 
+USER_CHOICE = '''Enter one of the following
+
+- 'b' to look at 5-star books
+- 'c' to look at the cheapest books
+- 'n' to just get the next available book on the catalogue
+- 'q' to quit
+
+Enter your choice: '''
+
+
 def print_best_books():
     best_books = sorted(books, key=lambda x: (x.rating * -1, x.price))[:10] # sort by rating in desc order and get top 10
     for book in best_books:
@@ -9,8 +19,27 @@ def print_cheapest_books():
     cheapest_books = sorted(books, key=lambda x: x.price * -1)[:10] # sort by rating in desc order and get top 10
     for book in cheapest_books:
         print(book)
+books_generator = (x for x in books)
 
-print('---- BEST BOOKS ----')
-print_best_books()
-print('---- CHEAPEST BOOKS ----')
-print_cheapest_books()
+def get_next_book():
+    print(next(books_generator))
+
+def menu():
+    user_input = input(USER_CHOICE)
+    while user_input != 'q':
+        if user_input == 'b':
+            print_best_books()
+        elif user_input == 'c':
+            print_cheapest_books()
+        elif user_input == 'n':
+            get_next_book()
+            # print(books[0])
+        else:
+            print('Unknown command.  Please try again.')
+        user_input = input(USER_CHOICE)
+
+menu()
+# print('---- BEST BOOKS ----')
+# print_best_books()
+# print('---- CHEAPEST BOOKS ----')
+# print_cheapest_books()

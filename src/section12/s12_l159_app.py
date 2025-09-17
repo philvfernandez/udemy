d849d8ecc0309a74029bec636695929e1d49b2db
Felipe.Fernandez <philvfernandez@gmail.com>
2025-09-17 14:01:38 -0600

    s11_l84 - Added new page locators for the quote search page.

--- src/section12/s12_l159_app.py
+++ src/section12/s12_l159_app.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from utils.common.pages.quotes_page import QuotesPageSelenium
 
 chrome = webdriver.Chrome()
-chrome.get('https://quotes.toscrape.com')
+chrome.get('https://quotes.toscrape.com/search.aspx')
 page = QuotesPageSelenium(chrome)
 
 for quote in page.quotespageselenium:
--- utils/common/locators/quote_locators.py
+++ utils/common/locators/quote_locators.py
@@ -2,3 +2,6 @@ class QuoteLocators:
     AUTHOR = 'span.text'
     CONTENT = 'small.author'
     TAGS = 'div.tags a.tag'
+    CONTENT_LOCATOR = 'span.content'
+    AUTHOR_LOCATOR = 'span.author'
+    TAGS_LOCATOR = 'span.tag'

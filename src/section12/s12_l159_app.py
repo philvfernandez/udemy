73b1b534cca55ee209c44f6a2dfde0d7209ff930
Felipe.Fernandez <philvfernandez@gmail.com>
2025-09-18 16:20:18 -0600

    s11_l189 - Added some error handling.

--- src/section12/s12_l159_app.py
+++ src/section12/s12_l159_app.py
@@ -1,16 +1,26 @@
 from selenium import webdriver
-from utils.common.pages.quotes_page import QuotesPageSelenium
+from utils.common.pages.quotes_page import QuotesPageSelenium, InvalidTagForAuthorError
 from utils.common.pages.quotes_page import QuotesPage
 
-author = input("Enter the author you'd like quotes from: ")
-tag = input("Enter your tag: ")
+try:
 
-chrome = webdriver.Chrome()
-chrome.get('https://quotes.toscrape.com/search.aspx')
-page = QuotesPageSelenium(chrome)
+    author = input("Enter the author you'd like quotes from: ")
+    tag = input("Enter your tag: ")
 
-#page.select_author(author)
-#page.select_tag(selected_tag)
-#page.search_button.click()
+    chrome = webdriver.Chrome()
+    chrome.get('https://quotes.toscrape.com/search.aspx')
+    page = QuotesPageSelenium(chrome)
 
-print(page.search_for_quotes(author, tag))
+    #page.select_author(author)
+    #page.select_tag(selected_tag)
+    #page.search_button.click()
+
+    print(page.search_for_quotes(author, tag))
+
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("An Unknown error occurred.  Please try again.")
+finally:
+    chrome.quit()
